485aa39fd128516fe987b72d3da9db827f205e51
Joy Kim <joykim@seoulchristian.org>
2017-01-11 10:42:46 +0900

    Enable to turn light on and off in response to commands sent through serial monitor

--- serialBlink/serialBlink.ino
+++ serialBlink/serialBlink.ino
@@ -1,6 +1,9 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial.println("Hello, how are you?");
+  Serial.println("Send 'On' to turn on the LED.");
+  Serial.println("Send 'Off' to turn off the LED.");
   pinMode(13, OUTPUT);
 }
 
