e4b5fb49cff6bef5cea67ce2b6d40fdd17000618
Joy Kim <joykim@seoulchristian.org>
2017-01-18 10:13:47 +0900

    Make LED light gradually increase and decrease

--- forFade/forFade.ino
+++ forFade/forFade.ino
@@ -3,5 +3,14 @@ void setup() {
   pinMode(9, OUTPUT);
 }
 void loop() {
-  
+  for (int lightValue = 0; lightValue <= 255; lightValue++) {
+    analogWrite(9, lightValue);
+    Serial.println(lightValue);
+    delay(10);
+  }
+  for (int lightValue = 255; lightValue >= 0; lightValue--) {
+    analogWrite(9, lightValue);
+    Serial.println(lightValue);
+    delay(10);
+  }
 }
