7d45532bdbec80d874bd3a94a887d9665a2ad412
Joy Kim <joykim@seoulchristian.org>
2017-01-19 10:22:42 +0900

    Set LED and serial monitor to output lightValue

--- ifFade/ifFade.ino
+++ ifFade/ifFade.ino
@@ -1,10 +1,16 @@
 int lightValue = 0;
 int delta = 1;
-void setup() {
-  Serial.begin(9600)
-  pinMode(9,OUTPUT)
-  }
-void loop() {
-  
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
+}
+void loop() {
+  lightValue = lightValue + delta;
+  if (lightValue <= 0 || lightValue >= 255) {
+    delta = -delta;
+  }
+  analogWrite(9, lightValue);
+  Serial.println(lightValue);
+  delay(10);
 }
