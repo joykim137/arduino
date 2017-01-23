7d260c9d798cae58c229a29974d7a466c61cfa9e
Joy Kim <joykim@seoulchristian.org>
2017-01-23 10:39:15 +0900

    Commented the codes in forFade and ifFade

--- forFade/forFade.ino
+++ forFade/forFade.ino
@@ -1,16 +1,16 @@
 void setup() {
-  Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  Serial.begin(9600); // activates serial connection in USB port and sets baud rate to 9600
+  pinMode(9, OUTPUT); // sets pin 9 to output mode
 }
 void loop() {
-  for (int lightValue = 0; lightValue <= 255; lightValue++) {
-    analogWrite(9, lightValue);
-    Serial.println(lightValue);
-    delay(10);
+  for (int lightValue = 0; lightValue <= 255; lightValue++) { // sets the initialization, test, and increment/decrement
+    analogWrite(9, lightValue); // sends munber of light value to pin 9 in analog form
+    Serial.println(lightValue); // makes serial monitor print light value of the light in serial monitor
+    delay(10); // waits ten milliseconds
   }
-  for (int lightValue = 255; lightValue >= 0; lightValue--) {
-    analogWrite(9, lightValue);
-    Serial.println(lightValue);
-    delay(10);
+  for (int lightValue = 255; lightValue >= 0; lightValue--) { // sets the initiralization, test, and increment/decrement
+    analogWrite(9, lightValue); // sends munber of light value to pin 9 in analog form
+    Serial.println(lightValue); // makes serial monitor print light value of the light in serial monitor
+    delay(10); // waits ten milliseconds
   }
 }
--- ifFade/ifFade.ino
+++ ifFade/ifFade.ino
@@ -1,16 +1,16 @@
-int lightValue = 0;
-int delta = 1;
+int lightValue = 0; // sets value of lightValue to 0
+int delta = 1; // sets value of delta to 1
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  Serial.begin(9600); // activates serial connection in USB port and sets baud rate to 9600
+  pinMode(9, OUTPUT); // sets pin 9 to output mode
 }
 void loop() {
-  lightValue = lightValue + delta;
-  if (lightValue <= 0 || lightValue >= 255) {
-    delta = -delta;
+  lightValue = lightValue + delta; // lightValue becomes lightValue + delta
+  if (lightValue <= 0 || lightValue >= 255) { // sets values that will make the lines beneath true
+    delta = -delta; // the variable becomes negative of the value of delta
   }
-  analogWrite(9, lightValue);
-  Serial.println(lightValue);
-  delay(10);
+  analogWrite(9, lightValue); // sends munber of light value to pin 9 in analog form
+  Serial.println(lightValue); // makes serial monitor print light value of lightValue on serial monitor
+  delay(10); // waits ten milliseconds
 }
